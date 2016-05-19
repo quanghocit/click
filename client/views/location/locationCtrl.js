2a2f21bb979dec0eaa4926f3031c8b564cc0f92c
Nguyễn Quang Học <quanghocit@gmail.com>
2016-05-19 16:46:40 +0700

    Update step 1.6

--- client/views/location/locationCtrl.js
+++ client/views/location/locationCtrl.js
@@ -1,3 +1,8 @@
 /**
  * Created by HocSoiCa on 5/19/2016.
  */
+angular
+  .module('app')
+  .controller('Location', ['$scope', 'Tracking', function ($scope, Tracking) {
+      console.log("Hello");
+  }]);
