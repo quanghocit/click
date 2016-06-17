81ef194603b4d4bcab0e87fb0719deed5e36683c
Nguyễn Quang Học <quanghocit@gmail.com>
2016-06-17 09:21:26 +0700

    Update

--- client/views/news/newsCtrl.js
+++ client/views/news/newsCtrl.js
@@ -6,4 +6,66 @@ angular
   .controller('News', ['$scope', 'Tracking', function ($scope, Tracking) {
     $('.none').addClass("hidden");
     $('.news').removeClass("hidden");
+
+    $scope.gridReport = {
+      rowHeight: 35,
+      enableGridMenu: true,
+      fastWatch: true
+    };
+
+    $scope.gridReport.columnDefs = [
+      { name: 'Campaign', field: 'campaign', headerCellClass: 'colorHeader', pinnedLeft: true},
+      { name: 'Client', field: 'client', headerCellClass: 'colorHeader'},
+      { name: 'Advertiser', field: 'advertiser', headerCellClass: 'colorHeader'},
+      { name: 'Type', field: 'type', headerCellClass: 'colorHeader'},
+      { name: 'Location', field: 'location', headerCellClass: 'colorHeader'},
+      { name: 'Schedule', field: 'schedule', headerCellClass: 'colorHeader'},
+      { name: 'Performance', field: 'performance', headerCellClass: 'colorHeader'},
+      { name: 'Action', field: 'action', headerCellClass: 'colorHeader'},
+    ];
+
+    $scope.gridReport.data = [
+      {
+        "campaign": "Yomost Summer",
+        "client": "FCV",
+        "advertiser": "Mindshare",
+        "type": "CPC",
+        "location": "Universities, Hồ Chí Minh",
+        "schedule": "03/06/2016 - 10/06/2016",
+        "performance": "20% KPI",
+        "action": "Approved"
+      },
+      {
+        "campaign": "Yomost Summer",
+        "client": "FCV",
+        "advertiser": "Mindshare",
+        "type": "CPC",
+        "location": "Universities, Hồ Chí Minh",
+        "schedule": "03/06/2016 - 10/06/2016",
+        "performance": "20% KPI",
+        "action": "Approved"
+      },
+      {
+        "campaign": "Yomost Summer",
+        "client": "FCV",
+        "advertiser": "Mindshare",
+        "type": "CPC",
+        "location": "Universities, Hồ Chí Minh",
+        "schedule": "03/06/2016 - 10/06/2016",
+        "performance": "20% KPI",
+        "action": "Approved"
+      },
+      {
+        "campaign": "Yomost Summer",
+        "client": "FCV",
+        "advertiser": "Mindshare",
+        "type": "CPC",
+        "location": "Universities, Hồ Chí Minh",
+        "schedule": "03/06/2016 - 10/06/2016",
+        "performance": "20% KPI",
+        "action": "Approved"
+      }
+    ];
+    var colHeight = $('.mainHotspot').height();
+    $("#gridHeight").css('height', colHeight - 70);
   }]);
